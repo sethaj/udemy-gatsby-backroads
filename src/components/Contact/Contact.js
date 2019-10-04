b80fb9cd40ff49636fa369dbf379256afca21cf3
Seth A. Johnson <sethajoh@umich.edu>
2019-10-04 13:50:04 -0400

    contact form

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Title from '../Title'
+import styles from '../../css/contact.module.css'
+
+const Contact = () => {
+  return (
+    <section className={styles.contact}>
+      <Title title="contact" subtitle="us" />
+      <div className={styles.center}>
+        <form action="" className={styles.form}>
+          <div>
+            <label htmlFor="name">name</label>
+            <input type="text"
+                   name="name"
+                   id="name"
+                   className={styles.formControl}
+                   placeholder="John Smith"/>
+          </div>
+          <div>
+            <label htmlFor="name">email</label>
+            <input type="email"
+                   name="email"
+                   id="email"
+                   className={styles.formControl}
+                   placeholder="email@email.com"/>
+          </div>
+          <div>
+            <label htmlFor="name">message</label>
+            <textarea name="message"
+                      id="message"
+                      rows="10"
+                      className={styles.formControl}
+                      placeholder="hello there"/>
+          </div>
+          <div>
+            <input type="submit" value="submit here" className={styles.submit} />
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
